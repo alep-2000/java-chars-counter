236d29c15e5d273e12a4e1854aae48c0b746191a
alep-2000 <alessiopiras056@gmail.com>
2023-11-17 15:18:37 +0100

    Richiesta all'utente della parola

--- src/org/lessons/java/charscounter/Main.java
+++ src/org/lessons/java/charscounter/Main.java
@@ -1,8 +1,16 @@
 package org.lessons.java.charscounter;
 
+
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+
+        System.out.println("Inserisci una parola: ");
+        String strParola = in.nextLine();
+
+        in.close();
         
 	}
 }
