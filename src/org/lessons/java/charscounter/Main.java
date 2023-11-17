0b76d587a4a0d7c534e4bc01e562540fb460cfaf
alep-2000 <alessiopiras056@gmail.com>
2023-11-17 15:19:49 +0100

    Creazione mappa, ciclo per recuperare le singole occorrenze e stampa in console chiave / valore

--- src/org/lessons/java/charscounter/Main.java
+++ src/org/lessons/java/charscounter/Main.java
@@ -1,6 +1,7 @@
 package org.lessons.java.charscounter;
 
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -12,5 +13,18 @@ public class Main {
 
         in.close();
         
+        
+        Map<Character, Integer> occorrenze = new HashMap<>();
+
+        for (int i = 0; i < strParola.length(); i++) {
+            char carattere = strParola.charAt(i);
+            occorrenze.put(carattere, occorrenze.getOrDefault(carattere, 0) + 1);
+        }
+
+       
+        System.out.println("Occorrenze dei caratteri nella parola:");
+        for (Map.Entry<Character, Integer> entry : occorrenze.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 	}
 }
